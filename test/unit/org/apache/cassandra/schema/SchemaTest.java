077b676ca37ae00f0c24c4dbb6859bc9ac1c119a
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2021-11-15 08:06:30 +0100

    fix unit tests

--- test/unit/org/apache/cassandra/schema/SchemaTest.java
+++ test/unit/org/apache/cassandra/schema/SchemaTest.java
@@ -19,35 +19,21 @@
 package org.apache.cassandra.schema;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Queue;
-import java.util.Set;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import org.apache.cassandra.SchemaLoader;
 import org.apache.cassandra.config.DatabaseDescriptor;
-import org.apache.cassandra.cql3.CQLTester;
 import org.apache.cassandra.db.Keyspace;
 import org.apache.cassandra.db.commitlog.CommitLog;
 import org.apache.cassandra.gms.Gossiper;
-import org.apache.cassandra.service.ClientWarn;
-import org.assertj.core.api.Assertions;
-import org.jboss.byteman.contrib.bmunit.BMRule;
-import org.jboss.byteman.contrib.bmunit.BMRules;
-import org.jboss.byteman.contrib.bmunit.BMUnitRunner;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-//@RunWith(BMUnitRunner.class)
-public class SchemaTest extends CQLTester
+public class SchemaTest
 {
     @BeforeClass
     public static void setupDatabaseDescriptor()
@@ -63,7 +49,7 @@ public class SchemaTest extends CQLTester
         Schema.instance.loadFromDisk();
         assertEquals(0, Schema.instance.getNonSystemKeyspaces().size());
 
-        Gossiper.instance.start((int) (System.currentTimeMillis() / 1000));
+        Gossiper.instance.start((int)(System.currentTimeMillis() / 1000));
         Keyspace.setInitialized();
 
         try
@@ -92,81 +78,4 @@ public class SchemaTest extends CQLTester
         }
     }
 
-    @Test
-    @BMRules(rules = { @BMRule(name = "client warning 1",
-                               targetClass = "CreateKeyspaceStatement",
-                               targetMethod = "apply",
-                               targetLocation = "AT INVOKE KeyspaceParams.validate",
-                               action = "org.apache.cassandra.schema.SchemaTest.addWarn()"),
-                       @BMRule(name = "client warning 2",
-                               targetClass = "CreateKeyspaceStatement",
-                               targetMethod = "clientWarnings",
-                               targetLocation = "AT EXIT",
-                               action = "return org.apache.cassandra.schema.SchemaTest.addWarnToList($!)"),
-                       @BMRule(name = "client warning 3",
-                               targetClass = "AlterSchemaStatement",
-                               targetMethod = "clientWarnings",
-                               targetLocation = "AT EXIT",
-                               action = "return org.apache.cassandra.schema.SchemaTest.addWarnToList($!)")})
-    public void testClientWarningsOnCreateKeyspace() throws Throwable
-    {
-        ClientWarn.instance.captureWarnings();
-        injectedWarnings.clear();
-        createKeyspace("CREATE KEYSPACE %s WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}");
-
-        Assertions.assertThat(injectedWarnings).contains(msg1, msg2); // failure here means the bm rules need to be updated
-        Assertions.assertThat(ClientWarn.instance.getWarnings()).containsExactlyInAnyOrder(msg1, msg2);
-    }
-
-    @Test
-    @BMRules(rules = { @BMRule(name = "client warning 1",
-                               targetClass = "CreateTableStatement",
-                               targetMethod = "apply",
-                               targetLocation = "AT INVOKE TableMetadata.validate",
-                               action = "org.apache.cassandra.schema.SchemaTest.addWarn()"),
-                       @BMRule(name = "client warning 2",
-                               targetClass = "CreateTableStatement",
-                               targetMethod = "clientWarnings",
-                               targetLocation = "AT EXIT",
-                               action = "return org.apache.cassandra.schema.SchemaTest.addWarnToList($!)"),
-                       @BMRule(name = "client warning 3",
-                               targetClass = "AlterSchemaStatement",
-                               targetMethod = "clientWarnings",
-                               targetLocation = "AT EXIT",
-                               action = "return org.apache.cassandra.schema.SchemaTest.addWarnToList($!)"),
-    })
-    public void testClientWarningsOnCreateTable() throws Throwable
-    {
-        ClientWarn.instance.captureWarnings();
-        injectedWarnings.clear();
-        createTable("CREATE TABLE %s (k int primary key, v int)");
-
-        Assertions.assertThat(injectedWarnings).contains(msg1, msg2); // failure here means the bm rules need to be updated
-        Assertions.assertThat(ClientWarn.instance.getWarnings()).containsExactlyInAnyOrder(msg1, msg2);
-    }
-
-    static volatile String msg1, msg2;
-
-    @Before
-    public void before()
-    {
-        msg1 = UUID.randomUUID().toString();
-        msg2 = UUID.randomUUID().toString();
-    }
-
-    static Queue<String> injectedWarnings = new ConcurrentLinkedQueue<String>();
-
-    public static void addWarn()
-    {
-        ClientWarn.instance.warn(msg1);
-        injectedWarnings.add(msg1);
-    }
-
-    public static Set<String> addWarnToList(Set<String> warns)
-    {
-        warns = new HashSet<>(warns);
-        warns.add(msg2);
-        injectedWarnings.add(msg2);
-        return warns;
-    }
 }
