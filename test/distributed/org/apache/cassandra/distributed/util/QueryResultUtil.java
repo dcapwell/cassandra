10ac8f81bd58fbb83702e3228a78b99b279dfe98
David Capwell <dcapwell@apache.org>
2022-08-30 11:08:51 -0700

    use constructor rather than pre-built array

--- test/distributed/org/apache/cassandra/distributed/util/QueryResultUtil.java
+++ test/distributed/org/apache/cassandra/distributed/util/QueryResultUtil.java
@@ -73,7 +73,7 @@ public class QueryResultUtil
             };
             rows[i] = row;
         }
-        return new SimpleQueryResult(input.names().toArray(new String[0]), rows, input.warnings());
+        return new SimpleQueryResult(input.names().toArray(String[]::new), rows, input.warnings());
     }
 
     public static boolean contains(SimpleQueryResult qr, Object... values)
