cfbfd3ab4302216500c0b40ef4212822f2293a06
David Capwell <dcapwell@apache.org>
2024-10-12 09:36:14 -0700

    updated test to reflect recent changes

--- test/unit/org/apache/cassandra/cql3/TableWalkTest.java
+++ test/unit/org/apache/cassandra/cql3/TableWalkTest.java
@@ -31,17 +31,18 @@ import accord.utils.Gens;
 import accord.utils.Property;
 import accord.utils.Property.SimpleCommand;
 import accord.utils.RandomSource;
+import org.apache.cassandra.cql3.ast.Bind;
 import org.apache.cassandra.cql3.ast.Mutation;
 import org.apache.cassandra.cql3.ast.Select;
 import org.apache.cassandra.cql3.ast.Symbol;
 import org.apache.cassandra.harry.model.ASTChecker;
+import org.apache.cassandra.harry.model.OpSelectors;
 import org.apache.cassandra.schema.KeyspaceMetadata;
 import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.utils.ASTGenerators.MutationGenBuilder;
 import org.quicktheories.generators.SourceDSL;
 
 import static accord.utils.Property.commands;
-import static accord.utils.Property.multistep;
 import static accord.utils.Property.stateful;
 import static org.apache.cassandra.utils.AbstractTypeGenerators.getTypeSupport;
 import static org.apache.cassandra.utils.Generators.toGen;
@@ -50,20 +51,24 @@ public class TableWalkTest extends CQLTester
 {
     public Property.Command<State, Void, ?> insert(RandomSource rs, State state)
     {
-        Mutation mutation = state.mutationGen.next(rs);
+        Mutation mutation = state.mutationGen.next(rs).withoutTTL().withoutTimestamp();
+        OpSelectors.OperationKind kind = state.checker.kind(mutation);
         Select select = select(mutation);
         long pd = state.checker.pd(mutation);
-        return multistep(new SimpleCommand<>(mutation.kind.name() + " " + pd, s2 -> {
-                             execute(mutation);
-                             s2.checker.update(mutation);
-                         }),
-                         new SimpleCommand<>("SELECT " + pd, s2 -> s2.checker.validate(select, getRows(execute(select)))));
+//        String name = kind.name() + " pd" + pd;
+        String name = mutation.toString();
+        return new SimpleCommand<>(name, s2 -> {
+            execute(mutation);
+            s2.checker.update(mutation);
+            s2.checker.validate(select, getRows(execute(select)));
+        });
     }
 
     @Test
     public void test()
     {
-        stateful().withSeed(3449305727684885768L).withExamples(1).withSteps(20).check(commands(() -> State::new)
+//        3449305727684885768L
+        stateful().withSeed(-7576457307656450664L).withExamples(100).withSteps(20).check(commands(() -> State::new)
                          .add(this::insert)
                          .build());
     }
@@ -78,34 +83,45 @@ public class TableWalkTest extends CQLTester
         {
             KeyspaceMetadata ks = createKeyspace(rs);
             this.metadata = createTable(rs, ks.name);
-            List<Map<Symbol, ByteBuffer>> uniqueValues;
+            List<Map<Symbol, Object>> uniqueValues;
             {
                 int unique = rs.nextInt(1, 10);
                 if (metadata.partitionKeyColumns().size() == 1)
                 {
                     Symbol col = Symbol.from(metadata.partitionKeyColumns().get(0));
-                    var bbs = Gens.lists(toGen(getTypeSupport(col.type()).bytesGen()))
-                        .uniqueBestEffort()
-                        .ofSize(unique)
-                    .next(rs);
+                    List<Object> bbs = Gens.lists(toGen(getTypeSupport(col.type()).valueGen).map(v -> (Object) v))
+                                      .uniqueBestEffort()
+                                      .ofSize(unique)
+                                      .next(rs);
                     uniqueValues = bbs.stream().map(b -> Map.of(col, b)).collect(Collectors.toList());
                 }
                 else
                 {
                     List<Symbol> columns = metadata.partitionKeyColumns().stream().map(Symbol::from).collect(Collectors.toList());
                     uniqueValues = Gens.lists(r2 -> {
-                        Map<Symbol, ByteBuffer> vs = new HashMap<>();
+                        Map<Symbol, Object> vs = new HashMap<>();
                         for (Symbol column : columns)
-                            vs.put(column, toGen(getTypeSupport(column.type()).bytesGen()).next(rs));
+                        {
+                            Object value = toGen(getTypeSupport(column.type()).valueGen).next(rs);
+                            vs.put(column, value);
+                        }
                         return vs;
                     }).uniqueBestEffort().ofSize(unique).next(rs);
                 }
             }
             this.checker = new ASTChecker(metadata);
             this.mutationGen = toGen(new MutationGenBuilder(metadata)
-                                                .withoutTransaction()
-                                                .withPartitions(SourceDSL.arbitrary().pick(uniqueValues))
-                                                .build());
+                                     .withoutTransaction()
+                                     .withPartitions(SourceDSL.arbitrary().pick(uniqueValues))
+                                     //TODO (coverage): there are known issues with literals, so this is to make the tests more stable
+                                     .withLiteralOrBindGen((value, type) -> i -> new Bind(value, type))
+                                     .build());
+        }
+
+        @Override
+        public String toString()
+        {
+            return metadata.toCqlString(false, false, false);
         }
     }
 }
